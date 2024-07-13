2343137eeb5e2eb3f8d39c8b87a6c9fb971d3e8a
Henry <henryng161@gmail.com>
2024-07-13 12:55:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const total = {
+  body: 17,
+  eyes: 17,
+  hair: 73,
+  mouth: 24,
+  eyebrows: 15,
+  glasses: 17,
+  clothing1: 5,
+  clothing2: 5,
+  clothing3: 9,
+};
+const body = [];
+for (let i = 0; i < total.body; i++) {
+  const path = `components/character/body/${i + 1}.png`;
+  body.push(path);
+  console.log(body);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>CHARACTER</h1>
+      <h2>Customization</h2>
+      <div className="divider"></div>
+      <div className="container">
+        <div className="avatar-wrapper">
+          <p>avatar component</p>
+          <button className="randomize-btn">RANDOMIZE!</button>
+        </div>
+        <div className="part-list-wrapper">
+          <div className="part-list">
+            <h2>Body</h2>
+            {body.map((bodyPart, i) => {
+              return (
+                <div key={i} style={{ width: "40%" }}>
+                  <img src={bodyPart} alt="here" />
+                </div>
+              );
+            })}
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+          <div className="part-list">
+            <h2>part-name</h2>
+            <p>part component here</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PartList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PartList() {
+  return <div>PartList</div>;
+}
+
+export default PartList;
